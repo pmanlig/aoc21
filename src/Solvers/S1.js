25369ad2431b8e64840a7a32690db6c242c471ef
pmanlig <patrik@manlig.org>
2021-12-02 05:18:05 +0100

    Day 1

--- src/Solvers/S1.js
+++ src/Solvers/S1.js
@@ -2,7 +2,17 @@ import Solver from './Solver';
 
 export class S1a extends Solver {
 	solve(input) {
-		this.setState({ solution: `No solution yet` });
+		// input = input.split('\n');
+		// input = input.split('\n').map(s => /^(\w+)$/.exec(s).splice(1));
+		input = input.split('\n').map(s => parseInt(s));
+		let count = 0, count2 = 0;
+		for (let i = 1; i < input.length; i++) {
+			if (input[i] > input[i - 1]) count++;
+		}
+		for (let j = 3; j < input.length; j++) {
+			if (input[j - 3] < input[j]) count2++;
+		}
+		this.setState({ solution: `Increases: ${count}\nSliding window increases: ${count2}` });
 	}
 }
 
