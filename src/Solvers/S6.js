a06b9fbe958975f2244d0220c07ad8fd70ddae0e
pmanlig <patrik@manlig.org>
2021-12-06 06:12:13 +0100

    Day 6

--- src/Solvers/S6.js
+++ src/Solvers/S6.js
@@ -2,10 +2,25 @@ import Solver from './Solver';
 
 export class S6a extends Solver {
 	solve(input) {
-		input = input.split('\n');
+		input = input.split(',').map(n => parseInt(n));
 		// input = input.map(s => /^(\w+)$/.exec(s).splice(1));
 		// input = input.map(s => parseInt(s));
-		this.setState({ solution: `No solution yet` });
+		let fish = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+		input.forEach(f => fish[f]++);
+		let numFish80;
+		for (let i = 0; i < 256; i++) {
+			if (i===80) {
+				numFish80 = fish.reduce((a, b) => a + b);
+			}
+			let breed = fish[0];
+			for (let n = 0; n < 9; n++) {
+				fish[n] = fish[n + 1];
+			}
+			fish[8] += breed;
+			fish[6] += breed;
+		}
+		let numFish256 = fish.reduce((a, b) => a + b);
+		this.setState({ solution: `Number of fish after 80 days: ${numFish80}\nNumber of fish after 256 days: ${numFish256}` });
 	}
 }
 
