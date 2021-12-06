6847d8d42efce17a7c65db5f90387ae9370f2b66
pmanlig <patrik@manlig.org>
2021-12-06 02:44:53 +0100

    Tidying

--- src/Solvers/S6.js
+++ src/Solvers/S6.js
@@ -2,6 +2,9 @@ import Solver from './Solver';
 
 export class S6a extends Solver {
 	solve(input) {
+		input = input.split('\n');
+		// input = input.map(s => /^(\w+)$/.exec(s).splice(1));
+		// input = input.map(s => parseInt(s));
 		this.setState({ solution: `No solution yet` });
 	}
 }
