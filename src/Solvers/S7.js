a712ce7fa711afbe44c3329b39eeee29bd87e246
pmanlig <patrik@manlig.org>
2021-12-08 05:55:20 +0100

    Day 7

--- src/Solvers/S7.js
+++ src/Solvers/S7.js
@@ -2,7 +2,20 @@ import Solver from './Solver';
 
 export class S7a extends Solver {
 	solve(input) {
-		this.setState({ solution: `No solution yet` });
+		// input = input.map(s => /^(\w+)$/.exec(s).splice(1));
+		input = input.split(',').map(s => parseInt(s));
+		input = input.sort((a, b) => a - b);
+		let target = [], target2 = [];
+		for (let t = input[0]; t <= input[input.length - 1]; t++) {
+			target[t] = 0;
+			target2[t] = 0;
+			for (let i = 0; i < input.length; i++) {
+				let dist = Math.abs(t - input[i])
+				target[t] += dist;
+				target2[t] += (dist + 1) * dist / 2;
+			}
+		}
+		this.setState({ solution: `Min: ${input[0]}\nMax: ${input[input.length - 1]}\nMinimum fuel(a): ${Math.min(...target)}\nMinimum fuel(b): ${Math.min(...target2)}` });
 	}
 }
 
